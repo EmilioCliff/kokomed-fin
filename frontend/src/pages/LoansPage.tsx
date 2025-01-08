ec6cc1d780169aeb576cc3a12880d5936a7fa4ed
EmilioCliff <emiliocliff@gmail.com>
2025-01-08 16:34:05 +0300

    add log

--- frontend/src/pages/LoansPage.tsx
+++ frontend/src/pages/LoansPage.tsx
@@ -66,7 +66,7 @@ export default function LoanPage() {
 		setLoading(false);
 	}, [validatedLoans]);
 
-	console.log("Here 2")
+	console.log("Here 3")
 
 	const fieldRenderers: Record<string, (value: any) => JSX.Element> = {
 		id: (value) => (
